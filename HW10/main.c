5f55e6e445124e275a9ea04167f3faab90d5e9d4
Klemens Iten <kiten@ethz.ch>
2023-05-04 11:32:02 -0500

    HW10 solid color changing

--- HW10/main.c
+++ HW10/main.c
@@ -5,14 +5,18 @@
 
 int main(void) {
   float heartbeat = 1000; // ms
+  float color_cycle = 2500; // ms
   NU32DIP_Startup(); // cache on, interrupts on, LED/button init, UART init
   ws2812b_setup();
   
   int numLEDs = 8;
-  wsColor white = HSBtoRGB(0,1,1); 
+  float hue = 0;
+  
+  wsColor white = HSBtoRGB(hue,1,1); 
 
   wsColor c[numLEDs]; 
   float t = _CP0_GET_COUNT();
+  float t_color = _CP0_GET_COUNT();
   
   for (int i=0; i<numLEDs; i++) {
       c[i] = white;
@@ -21,6 +25,15 @@ int main(void) {
   while (1) {
       
     ws2812b_setColor(&white, numLEDs);
+    
+    if (_CP0_GET_COUNT() > t_color + 24000*(color_cycle/360)) {
+        t_color = _CP0_GET_COUNT();
+        hue = hue+1;
+        if (hue >= 359) {
+            hue = 0;
+        }
+        white = HSBtoRGB(hue,1,1);
+    }
     
     if (_CP0_GET_COUNT() > t + 24000*heartbeat) {
         NU32DIP_GREEN = !NU32DIP_GREEN; // switches heartbeat lamp

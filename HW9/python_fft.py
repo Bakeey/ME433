5a6a6861028dcaae2b1b08af932885d010f3c7b4
Klemens Iten <kiten@ethz.ch>
2023-05-08 11:44:57 -0500

    HW9 done

--- HW9/python_fft.py
+++ HW9/python_fft.py
@@ -1,28 +1,156 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 import numpy as np
+import csv
+"""
+Some helpful basic signal processing functions, 
+made by Klemens Iten for ME433 Advanced Mechatronics at Northwestern
+based on templates by Nick Marchuk
+"""
 
-dt = 1.0/10000.0 # 10kHz
-t = np.arange(0.0, 1.0, dt) # 10s
-# a constant plus 100Hz and 1000Hz
-s = 4.0 * np.sin(2 * np.pi * 100 * t) + 0.25 * np.sin(2 * np.pi * 1000 * t) + 25
+def plot_fft(t,y,frq,Y,title = None):
+    """
+    Plots signal over time as well as frequency spectrum over frequency range.
+    Inputs:
+        t (time as list), 
+        data (signal data as list)
+        frq (one side frequency range of signal), 
+        Y (fft of signal)
+    """
+    fig, (ax1, ax2) = plt.subplots(2, 1)
+    if title is not None:
+        fig.suptitle(title, fontsize=16)
+    ax1.plot(t,y,'b')
+    ax1.set_xlabel('Time')
+    ax1.set_ylabel('Amplitude')
+    ax2.loglog(frq,abs(Y),'b') # plotting the fft
+    ax2.set_xlabel('Freq (Hz)')
+    ax2.set_ylabel('|Y(freq)|')
+    plt.show()
 
-Fs = 10000 # sample rate
-Ts = 1.0/Fs; # sampling interval
-ts = np.arange(0,t[-1],Ts) # time vector
-y = s # the data to make the fft from
-n = len(y) # length of the signal
-k = np.arange(n)
-T = n/Fs
-frq = k/T # two sides frequency range
-frq = frq[range(int(n/2))] # one side frequency range
-Y = np.fft.fft(y)/n # fft computing and normalization
-Y = Y[range(int(n/2))]
+def fft(t, data):
+    """
+    Computes FFT of some given function.
+    Inputs: 
+        t (time as list), 
+        data (signal data as list)
+    Returns: 
+        frq (one side frequency range of signal), 
+        Y (fft of signal)
+    """
+    dt = t[-1] / len(t)
+    Fs = 1/dt
+    n = len(data) # length of the signal
+    k = np.arange(n)
+    T = n/Fs
+    frq = k/T # two sides frequency range
+    frq = frq[range(int(n/2))] # one side frequency range
+    Y = np.fft.fft(data)/n # fft computing and normalization
+    Y = Y[range(int(n/2))]
+    return frq,Y
 
-fig, (ax1, ax2) = plt.subplots(2, 1)
-ax1.plot(t,y,'b')
-ax1.set_xlabel('Time')
-ax1.set_ylabel('Amplitude')
-ax2.loglog(frq,abs(Y),'b') # plotting the fft
-ax2.set_xlabel('Freq (Hz)')
-ax2.set_ylabel('|Y(freq)|')
-plt.show()
+def plot_maf(t, data, data_maf, n, title = None):
+    if title is not None:
+        plt.title(f"{title} MAF averaged over {n} samples", fontsize=16)
+    plt.plot(t,data,'k')
+    plt.plot(t,data_maf,'r')
+    plt.xlabel('Time')
+    plt.ylabel('Amplitude')
+    plt.show()
+
+    frq, Y = fft(t, data_maf)
+    plot_fft(t,data_maf,frq,Y, title=f"{title} MAF averaged over {n} samples")
+
+def maf(t, data, n):
+    """
+    Performs moving average filter over some data and returns smoothed data.
+    Inputs:
+        t (time as list), 
+        data (signal data as list)
+        n (number of datapoints to average on)
+    Returns: 
+        list of smoothed data with delay as leading zeros
+    """
+    data_maf = [0]*(n-1)
+    for idx,_ in enumerate(data[n-1::]):
+        window = data[idx:idx+n-1]
+        data_maf.append(np.average(window))
+    return data_maf
+
+def plot_iir(t, data, data_iir, A, title = None):
+    if title is not None:
+        plt.title(f"{title} IIR smoothed with A={A}, B={1-A}", fontsize=16)
+    plt.plot(t,data,'k')
+    plt.plot(t,data_iir,'r')
+    plt.xlabel('Time')
+    plt.ylabel('Amplitude')
+    plt.show()
+
+def iir(t, data, A):
+    """
+    Performs infinite impulse filter over some data and returns smoothed data.
+    Inputs:
+        t (time as list), 
+        data (signal data as list)
+        A (weight of new datapoint <1)
+    Returns: 
+        list of smoothed data
+    """
+    assert(0<=A<=1, f"expected A in [0,1], got: {A}")
+    B = 1 - A
+    data_iir = data[0:1:]
+    running_average = data[0]
+    for datapoint in data[1::]:
+        data_iir.append( A * datapoint + B * running_average)
+        running_average = data_iir[-1]
+    return data_iir
+
+def fir(t, data):
+    # Configuration.
+    fS = 10000.20000400008  # Sampling rate.
+    fH = 100.00200004000081  # Cutoff frequency.
+    N = 47  # Filter length, must be odd.
+    # Compute sinc filter.
+    h = np.sinc(2 * fH / fS * (np.arange(N) - (N - 1) / 2))
+    # Apply window.
+    h *= np.blackman(N)
+    # Normalize to get unity gain.
+    h /= np.sum(h)
+    # Create a high-pass filter from the low-pass filter through spectral inversion.
+    h = -h
+    h[(N - 1) // 2] += 1
+
+    s = np.convolve(data, h)
+    frq, Y = fft(t, data)
+    plot_fft(t, data, frq, Y)
+
+def main():
+    """
+    Main Loop
+    """
+    for filename in ['sigD.csv']: # ['sigA.csv','sigB.csv','sigC.csv','sigD.csv']:
+        t = []
+        data = []
+        with open(filename) as f:
+            # open the csv file
+            reader = csv.reader(f)
+            for row in reader:
+                # read the rows 1 one by one
+                t.append(float(row[0])) # leftmost column
+                data.append(float(row[1])) # second column
+        frq, Y = fft(t, data)
+        plot_fft(t,data,frq,Y, title=filename[:-4])
+
+        for moving_average in [5,10,25,50,100,250,500,1000]:
+            data_maf = maf(t, data, moving_average)
+            plot_maf(t, data, data_maf, moving_average, title=filename[:-4])
+
+        for A in [0, 0.01, 0.02, 0.05, 0.1, 0.25]:
+            data_iir = iir(t, data, A)
+            plot_iir(t, data, data_iir, A, title=filename[:-4])
+
+        sampling_rate = len(t) / t[-1]
+
+if __name__ == "__main__":
+    exit(main())
